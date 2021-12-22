d875a3ea5a67502e6649b79ec4f190086d16e410
Junaid Aslam <junaidaslam.muet@gmail.com>
2021-12-22 20:56:20 +0500

    Update Current.java

--- Current.java
+++ Current.java
@@ -1,4 +1,5 @@
 package jonnyCrosswords;
+
 import java.awt.*;
 import java.io.*;
 import javax.swing.*;
@@ -16,7 +17,7 @@ public class GUI {
 		jf.setLayout(null);
 		jp1=new JPanel();
 //		jp1.setSize(200,450);
-		jp1.setBounds(2,3,200,450);
+		jp1.setBounds(2,3,200,480);
 		jp1.setBorder(BorderFactory.createLineBorder(Color.black));
 		jp1.setVisible(true);
 		jp2=new JPanel();
@@ -34,24 +35,117 @@ public class GUI {
 		jb1.setText("Check");
 		jb1.setFont(new Font(null,Font.PLAIN,6));
 		jb1.setVisible(true);
+		GridLayout gl=new GridLayout(13,13);
+
+		JTextField[][] jtf2=new JTextField[13][13];	
+		
+		GridLayout gl2=new GridLayout(1,9);
+		jp3.setLayout(gl2);
+		JTextField[][] jtf3=new JTextField[1][9];	
+		
+		
+		JTextArea text=new JTextArea("Crossword Puzzle Instructions:\n");
+		text.setFont(new Font(null,Font.PLAIN,9));
+		text.setBounds(5,5,200,550);
+		
+		for(int i=0;i<13;i++) {
+			for(int j=0;j<13;j++) {
+				jtf2[i][j]=new JTextField();
+			}
+		}
+		
+		for(int i=0;i<1;i++) {
+			for(int j=0;j<9;j++) {
+				jtf3[i][j]=new JTextField();
+				jtf3[i][j].setFont(new Font(null, Font.PLAIN, 30));
+				jtf3[i][j].setBackground(Color.gray);
+			}
+		}
 		
 		try{
+			String line=null;
+            int j=0;
             fs=new FileReader("C:/Users/hp/eclipse-workspace/jonnyCrosswords/src/puzzle.txt");
             BufferedReader br=new BufferedReader(fs);
             String[] dimensions = br.readLine().split(" ");
-            char[][] letterGrid = new char[Integer.parseInt(dimensions[0])][Integer.parseInt(dimensions[1])];
-            String line=null;
+//            char[][] letterGrid = new char[Integer.parseInt(dimensions[0])][Integer.parseInt(dimensions[1])];
+//            System.out.println(br.readLine());
+            
             while ((line=br.readLine())!=null) {
+//            	System.out.println(line);
             	String[] str=line.split(" ",13);
-            	for(int i=0;i<str.length;i++) {
-            		
+            	
+            	for(int i=0;i<13;i++) {
+            		if(str[i].equals("O")) {
+            			jtf2[j][i].setFont(new Font("SansSerif", Font.BOLD, 30));
+            			jtf2[j][i].setBackground(Color.WHITE);
+            		}
+            		else if(str[i].equals("X")) {
+            			jtf2[j][i].setBackground(Color.BLACK);
+            			jtf2[j][i].setForeground(Color.BLACK);
+            			jtf2[j][i].setFont(new Font("SansSerif", Font.BOLD, 30));
+            		}
+            		else if(str[i].equals("S")) {
+            			jtf2[j][i].setBackground(Color.GRAY);
+            			jtf2[j][i].setFont(new Font("SansSerif", Font.BOLD, 30));
+            		}
+            		else if(str[i].equals("H(H)") ){
+            			jtf2[j][i].setBackground(Color.decode("#87ceeb"));
+            			jtf2[j][i].setFont(new Font("SansSerif", Font.BOLD, 30));
+            		}
+            		else if(str[i].equals("H(C)")) {
+            			(jtf2[j][i]).setBackground(Color.decode("#87ceeb"));
+            			jtf2[j][i].setFont(new Font("SansSerif", Font.BOLD, 30));
+            		}
+            		else if(str[i].equals("H(Y)") ){
+            			jtf2[j][i].setBackground(Color.decode("#87ceeb"));
+            			jtf2[j][i].setFont(new Font("SansSerif", Font.BOLD, 30));
+            		}
             	}
+            	j++;
             }
+            br.close();
+            fs.close();
 
 			}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
+		
+		try{
+			String line=null;
+            int j=0;
+            fs=new FileReader("C:/Users/hp/eclipse-workspace/jonnyCrosswords/src/Word.txt");
+            BufferedReader br=new BufferedReader(fs);
+            String[] dimensions = br.readLine().split(" ");
+//            char[][] letterGrid = new char[Integer.parseInt(dimensions[0])][Integer.parseInt(dimensions[1])];
+            
+            while ((line=br.readLine())!=null) {
+            	text.setText(text.getText()+ line+"\n");
+            }
+            br.close();
+            fs.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		jp2.setLayout(gl);
+
+		for(int i=0;i<13;i++) {
+			for(int j=0;j<13;j++) {
+				jp2.add(jtf2[i][j]);
+			}
+		}
+		jp1.add(text);
+		
+		for(int i=0;i<1;i++) {
+			for(int j=0;j<9;j++) {
+				jp3.add(jtf3[i][j]);
+			}
+		}
+		
+		
 		jf.add(jp1);
 		jf.add(jp2);
 		jf.add(jp3);
